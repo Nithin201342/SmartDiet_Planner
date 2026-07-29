10496a7fcd0df8827a420f86c5851dea27767237
Allen Albert <144606434+allen6996@users.noreply.github.com>
2026-07-29 16:29:29 +0530

    Fix import statement for PyMongo in app.py

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 from flask import Flask, render_template, request, redirect, url_for, flash, jsonify
-from flask_pymongo import Pymongo
+from flask_pymongo import PyMongo
 from flask_login import LoginManager, UserMixin, login_user, login_required, logout_user, current_user
 from werkzeug.security import generate_password_hash, check_password_hash
 from bson.objectid import ObjectId
